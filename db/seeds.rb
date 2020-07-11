d9db705ae38ab24fef6b5f2f3c47ca1673e94442
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-11 17:06:39 -0600

    Add from_csv method to importable module

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.from_csv('db/csv_seeds/customers.csv')
+
+Merchant.from_csv('db/csv_seeds/merchants.csv')
+
+Invoice.from_csv('db/csv_seeds/invoices.csv')
+
+Item.from_csv('db/csv_seeds/items.csv')
+
+InvoiceItem.from_csv('db/csv_seeds/invoice_items.csv')
+
+Transaction.from_csv('db/csv_seeds/transactions.csv')
