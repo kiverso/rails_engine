d0c65a2ad25d39a5f4c27c6e278e85d063330f4f
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-11 15:12:13 -0600

    Update migrations and create db schema

--- db/migrate/20200711184032_create_items.rb
+++ db/migrate/20200711184032_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string :name
       t.string :description
-      t.decimal{10 :unit_price
+      t.decimal :unit_price, precision: 10, scale: 2
       t.references :merchant, foreign_key: true
 
       t.timestamps
