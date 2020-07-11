8479cb4ccafa2572d12608166b33cdceb4d2610e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-11 15:13:35 -0600

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module RailsEngine
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
