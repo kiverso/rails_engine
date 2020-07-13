dd87d0f65ab010d39041bc25f1081d3ebe059104
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-13 17:19:01 -0600

    add merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show]
+      
+      resources :merchants, only: [:index, :show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
