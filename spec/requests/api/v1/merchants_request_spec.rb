e301a6014e8de3ac69042f60a153310b07d07297
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-13 17:25:55 -0600

    Add test for merchant index and show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,19 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Merchants", type: :request do
-
+  before(:each) do
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant2 = create(:merchant)
+  end
   describe "GET /index" do
     it "returns http success" do
       get "/api/v1/merchants"
       expect(response).to have_http_status(:success)
     end
+
+    it 'returns all merchants' do
+      get "/api/v1/merchants"
+      expect(response).to have_http_status(:success)
+
+      merchants = JSON.parse(response.body)
+      expect(merchants['data'].count).to eq(4)
+    end
   end
 
-  # describe "GET /show" do
-  #   it "returns http success" do
-  #     get "/api/v1/merchants/show"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /show" do
+    it "returns http success" do
+      get "/api/v1/merchants/#{@merchant1.id}"
+      expect(response).to have_http_status(:success)
+    end
 
+    it "returns the merchant" do
+      get "/api/v1/merchants/#{@merchant1.id}"
+
+      merchant = JSON.parse(response.body)
+      
+      expect(merchant['data']['attributes']['id']).to eq(@merchant1.id)
+      expect(merchant['data']['attributes']['name']).to eq(@merchant1.name)
+    end
+  end
 end
+
+
+
+
