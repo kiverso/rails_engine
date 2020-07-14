8f844fa048048917e9a910bae889b0e742180aaa
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-13 20:59:22 -0600

    add merchant create action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def create
+    new_merchant = Merchant.create!(merchant_params)
+    render json: MerchantSerializer.new(new_merchant)
+  end
+
+  private
+  def merchant_params
+    params.permit(:name)
+  end
 end
