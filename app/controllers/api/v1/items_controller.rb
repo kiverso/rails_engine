1d9d66eaaf35326f2bf9fc8480392feef793a203
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-13 19:58:55 -0600

    add item create controller action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,16 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    new_item = Item.create(item_params)
+    if new_item.save
+      render json: ItemSerializer.new(new_item)
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show]
-      
+      resources :items, only: [:index, :show, :create]
+
       resources :merchants, only: [:index, :show]
     end
   end
