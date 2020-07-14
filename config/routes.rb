85a24141b965b57b5bd7c10a2174b1de7d7a55d7
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-14 17:37:45 -0600

    add merchant_id param for items index action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :destroy, :update]
 
-      resources :merchants, only: [:index, :show, :create, :destroy, :update]
+      resources :merchants, only: [:index, :show, :create, :destroy, :update] do
+        resources :items, only: [:index]
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
