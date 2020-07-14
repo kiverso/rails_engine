70cdba5c9e35e9f6702c5730375183fbe687438e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-13 21:00:44 -0600

    add merchant destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :destroy]
 
-      resources :merchants, only: [:index, :show, :create]
+      resources :merchants, only: [:index, :show, :create, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
