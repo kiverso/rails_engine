ffedfedc53e4a9db7e9dc89340b5fe63a9715488
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-15 15:37:11 -0600

    add search index action

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
+    @merchants = Merchant.where(nil)
+    @merchants = @merchants.search_by_name(params[:name]) if params[:name]
+    @merchants = @merchants.search_by_created_at(params[:created_at]) if params[:created_at]
+    @merchants = @merchants.search_by_updated_at(params[:updated_at]) if params[:updated_at]
+    render json: MerchantSerializer.new(@merchants)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find', to: 'search#index'
+        get '/find_all', to: 'search#index'
       end
     end
   end
