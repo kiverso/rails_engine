13f780b162a36b7d09e245cf46d9b33cb4e66acc
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-16 00:35:47 -0600

    fix revenue index action

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RevenueController < ApplicationController
   def index
+    revenue = Invoice.revenue_between(params[:start], params[:end])
+    render json: RevenueSerializer.new(Revenue.new(revenue))
   end
 end
--- app/poros/revenue.rb
+++ app/poros/revenue.rb
@@ -2,6 +2,6 @@ class Revenue
   attr_reader :id, :revenue
   def initialize(revenue)
     @id = nil
-    @revenue = revenue
+    @revenue = revenue.to_f
   end
 end
